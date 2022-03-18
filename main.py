9686420c71d3c2b53acb383ec468a9fc2beeca90
FaikYY <yazilimcifaik@gmail.com>
2022-03-17 23:14:52 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import converter
+
+def main():
+    print('Welcome to converter Faik')
+    converter.calculate_pound_to_kg()
+    print("################")
+    converter.calculate_kg_to_pound()
+
+main()
